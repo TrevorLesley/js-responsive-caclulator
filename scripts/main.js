a32abc6edc51924219d7a11e81b5814bdc93fb30
Trevor Lesley <tlesley96@gmail.com>
2021-05-08 11:28:57 -0400

    Built the pushNumber function.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
+// Test alert.
+
+// window.alert('Hello.');
+
+// function pushNumber() {
+//     window.alert
+// }
+
+// Defining the pushNumber function to select all numbers,
+// also adding event listener to the function.
+
+const $numberButtons = document.querySelectorAll('.number');
+
+function pushNumber(event) {
+    alert(event.target.value);
+}
+
+$numberButtons.forEach(function (button) {
+    button.addEventListener('click', pushNumber);
+});
 
