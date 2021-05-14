2a8a2d56db9f9bc49aeaf78562b1d60f696eecb4
Trevor Lesley <tlesley96@gmail.com>
2021-05-14 11:23:01 -0400

    Corrected code to complete assignment.

--- scripts/main.js
+++ scripts/main.js
@@ -24,25 +24,20 @@ var calcString = [];
 
 
 function pushNumber(event) {
-    // alert(event.target.value);
-    console.log(event.target.value);
+    alert(event.target.value);
+    // console.log(event.target.value);
     document.querySelector('.calculator-screen').value = event.target.value;
     calculation.push(event.target.value);
     console.log(calculation);
-    // console.log(typeof (event.target.value));
-
 }
 
 $numberButtons.forEach(function (button) {
     button.addEventListener('click', pushNumber);
-    var toInt = parseInt(button);
-
-
 });
 
 function pushOperator(event) {
-    // alert(event.target.value);
-    console.log(event.target.value);
+    alert(event.target.value);
+    // console.log(event.target.value);
     calculation.push(event.target.value);
     document.querySelector('.calculator-screen').value = event.target.value;
 }
@@ -52,43 +47,38 @@ $operatorButtons.forEach(function (button) {
 });
 
 function calculate(calculation) {
-    let stringNum1 = '', stringNum2 = '', operator = '';
-    // let num1 = [], num2 = [];
-    for (i = 0; i < calculation.length; i++) {
-        if (calculation[i] === 'number') {
-            calculation.push(stringNum1);
-        } else if (calculation[i] === operator) {
-            calculation.push(operator);
-        } else if (calculation.indexOf[i] > operator.indexOf[i]) {
-            calculation.push(stringNum2);
+    alert('=');
+    let num1 = '', num2 = '', operator = null;
+    let operators = ['*', '+', '-', '/']
+
+    for (let i = 0; i < calculation.length; i++) {
+        const char = calculation[i];
+
+        if (operators.includes(char)) {
+            operator = char;
+        } else if (!operator) {
+            num1 += char;
+        } else {
+            num2 += char;
         }
-    };
-    stringNum1
-        .join()
-        .parseInt();
-
-    stringNum2
-        .join()
-        .parseInt();
-
-    if (operator == '+') {
-        console.log(stringNum1 + stringNum2);
-    } else if (operator == '-') {
-        console.log(stringNum1 - stringNum2);
-    } else if (operator == '*') {
-        console.log(stringNum1 * stringNum2);
-    } else if (operator == '/') {
-        console.log(stringNum1 / stringNum2);
     }
 
-    $equalSign.forEach(function (button) {
-        button.addEventListener('click', calculate);
-    });
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if (operator === '/') {
+        return num1 / num2;
+    }
 };
 
+$equalSign.addEventListener('click', calculate);
 
-
-// calculate(calculation);
 
 //  Clear button.
 
